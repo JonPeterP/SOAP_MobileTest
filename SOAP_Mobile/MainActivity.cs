6315497b54fce06eb0bf9c1212917407643646e3
Jon Peter Pomperada <55144487+JonPeterP@users.noreply.github.com>
2024-06-20 08:04:36 +0800

    Initial commit

---
+++ SOAP_Mobile/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace SOAP_Mobile
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView text1;
+        Button btnSubmit;
+        EditText edit1;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            btnSubmit = FindViewById<Button>(Resource.Id.button1);
+            btnSubmit.Click += BtnSubmitOnClick;
+
+            text1 = FindViewById<TextView>(Resource.Id.textView1);
+            edit1 = FindViewById<EditText>(Resource.Id.editText1);
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void BtnSubmitOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            //text1.setText("Button Clicked");
+            
+            com.daehosting.webservices.TemperatureConversions tempC = new com.daehosting.webservices.TemperatureConversions();
+            text1.Text = tempC.CelsiusToFahrenheit(decimal.Parse(edit1.Text)).ToString();
+
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
